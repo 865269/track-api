f2a4300066f118451ee8ffe75658327b20348f21
Matthew <mattydiggory@gmail.com>
2021-05-23 09:09:08 +0100

    first commit

---
+++ src/test/java/com/matthew/track/TrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthew.track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
