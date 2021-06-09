8203ec6057201cc94fbac902d320d61bf75f8882
Matthew <mattydiggory@gmail.com>
2021-06-09 19:22:55 +0100

    added unit tests for activity repo

--- src/test/java/com/matthew/track/TrackApplicationTests.java
+++ src/test/java/com/matthew/track/TrackApplicationTests.java
@@ -3,7 +3,6 @@ package com.matthew.track;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TrackApplicationTests {
 
 	@Test
